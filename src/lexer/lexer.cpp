fc04396a6974d8f00ed616be2449d0232c4d9094
Kinleoapple <996711203@qq.com>
2025-10-24 10:51:24 +1100

    Updated.

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -2,88 +2,101 @@
 #include <cctype>
 #include <unordered_map>
 
-namespace pawc {
-
-static const std::unordered_map<std::string, TokenType> keywords = {
-    {"fn", TokenType::KW_FN},
-    {"let", TokenType::KW_LET},
-    {"mut", TokenType::KW_MUT},
-    {"type", TokenType::KW_TYPE},
-    {"struct", TokenType::KW_STRUCT},
-    {"enum", TokenType::KW_ENUM},
-    // {"impl", TokenType::KW_IMPL},  // Deprecated: methods are now defined directly in struct
-    {"if", TokenType::KW_IF},
-    {"else", TokenType::KW_ELSE},
-    {"loop", TokenType::KW_LOOP},
-    {"break", TokenType::KW_BREAK},
-    {"continue", TokenType::KW_CONTINUE},
-    {"return", TokenType::KW_RETURN},
+namespace pawc
+{
+    static const std::unordered_map<std::string, TokenType> keywords = {
+        {"fn", TokenType::KW_FN},
+        {"let", TokenType::KW_LET},
+        {"mut", TokenType::KW_MUT},
+        {"type", TokenType::KW_TYPE},
+        {"struct", TokenType::KW_STRUCT},
+        {"enum", TokenType::KW_ENUM},
+        // {"impl", TokenType::KW_IMPL},  // Deprecated: methods are now defined directly in struct
+        {"if", TokenType::KW_IF},
+        {"else", TokenType::KW_ELSE},
+        {"loop", TokenType::KW_LOOP},
+        {"break", TokenType::KW_BREAK},
+        {"continue", TokenType::KW_CONTINUE},
+        {"return", TokenType::KW_RETURN},
         {"pub", TokenType::KW_PUB},
         {"import", TokenType::KW_IMPORT},
         {"extern", TokenType::KW_EXTERN},
-    {"self", TokenType::KW_SELF},
-    {"Self", TokenType::KW_SELF_TYPE},
-    {"true", TokenType::KW_TRUE},
-    {"false", TokenType::KW_FALSE},
-    {"in", TokenType::KW_IN},
-    {"is", TokenType::KW_IS},
-    {"as", TokenType::KW_AS},
-    {"ok", TokenType::KW_OK},
-    {"err", TokenType::KW_ERR}
-};
+        {"self", TokenType::KW_SELF},
+        {"Self", TokenType::KW_SELF_TYPE},
+        {"true", TokenType::KW_TRUE},
+        {"false", TokenType::KW_FALSE},
+        {"in", TokenType::KW_IN},
+        {"is", TokenType::KW_IS},
+        {"as", TokenType::KW_AS},
+        {"ok", TokenType::KW_OK},
+        {"err", TokenType::KW_ERR}
+    };
 
-Lexer::Lexer(const std::string& source, const std::string& filename)
-    : source_(source), filename_(filename), current_(0), line_(1), column_(1) {}
+    Lexer::Lexer(const std::string& source, const std::string& filename)
+        : source_(source), filename_(filename), current_(0), line_(1), column_(1)
+    {
+    }
 
-std::vector<Token> Lexer::tokenize() {
-    std::vector<Token> tokens;
-    while (!isAtEnd()) {
-        Token token = nextToken();
-        if (token.type != TokenType::INVALID) {
-            tokens.push_back(token);
+    std::vector<Token> Lexer::tokenize()
+    {
+        std::vector<Token> tokens;
+        while (!isAtEnd())
+        {
+            Token token = nextToken();
+            if (token.type != TokenType::INVALID)
+            {
+                tokens.push_back(token);
+            }
+            if (token.type == TokenType::END_OF_FILE)
+            {
+                break;
+            }
         }
-        if (token.type == TokenType::END_OF_FILE) {
-            break;
-        }
+        return tokens;
     }
-    return tokens;
-}
 
-Token Lexer::nextToken() {
-    skipWhitespace();
-    
-    if (isAtEnd()) {
-        return makeToken(TokenType::END_OF_FILE, "");
-    }
-    
-    char c = advance();
-    
-    // Identifier or keyword
-    if (std::isalpha(c) || c == '_') {
-        current_--;
-        column_--;
-        return identifier();
-    }
-    
-    // Number
-    if (std::isdigit(c)) {
-        current_--;
-        column_--;
-        return number();
-    }
-    
-    // String
-    if (c == '"') {
-        return string();
-    }
-    
-    // Character
-    if (c == '\'') {
-        return charLiteral();
-    }
-    
-    // Operators and delimiters
-    switch (c) {
+    Token Lexer::nextToken()
+    {
+        skipWhitespace();
+
+        if (isAtEnd())
+        {
+            return makeToken(TokenType::END_OF_FILE, "");
+        }
+
+        char c = advance();
+
+        // Identifier or keyword
+        if (std::isalpha(c) || c == '_')
+        {
+            current_--;
+            column_--;
+            return identifier();
+        }
+
+        // Number
+        if (std::isdigit(c))
+        {
+            current_--;
+            column_--;
+            return number();
+        }
+
+        // String
+        if (c == '"')
+        {
+            return string();
+        }
+
+        // Character
+        if (c == '\'')
+        {
+            return charLiteral();
+        }
+
+        // Operators and delimiters
+        switch (c)
+        {
         case '+':
             return makeToken(match('=') ? TokenType::PLUS_EQ : TokenType::PLUS, std::string(1, c));
         case '-':
@@ -92,7 +105,8 @@ Token Lexer::nextToken() {
             return makeToken(TokenType::MINUS, "-");
         case '*': return makeToken(TokenType::STAR, "*");
         case '/':
-            if (peek() == '/') {
+            if (peek() == '/')
+            {
                 skipComment();
                 return nextToken();
             }
@@ -127,189 +141,233 @@ Token Lexer::nextToken() {
         case ':':
             return makeToken(match(':') ? TokenType::DOUBLE_COLON : TokenType::COLON, ":");
         case '.':
-            if (peek() == '.') {
+            if (peek() == '.')
+            {
                 advance();
                 return makeToken(TokenType::DOTDOT, "..");
             }
             return makeToken(TokenType::DOT, ".");
+        }
+
+        return makeToken(TokenType::INVALID, std::string(1, c));
     }
-    
-    return makeToken(TokenType::INVALID, std::string(1, c));
-}
 
-Token Lexer::peekToken() {
-    size_t saved_current = current_;
-    int saved_line = line_;
-    int saved_column = column_;
-    
-    Token token = nextToken();
-    
-    current_ = saved_current;
-    line_ = saved_line;
-    column_ = saved_column;
-    
-    return token;
-}
+    Token Lexer::peekToken()
+    {
+        size_t saved_current = current_;
+        int saved_line = line_;
+        int saved_column = column_;
 
-bool Lexer::isAtEnd() const {
-    return current_ >= source_.length();
-}
+        Token token = nextToken();
 
-char Lexer::advance() {
-    column_++;
-    return source_[current_++];
-}
+        current_ = saved_current;
+        line_ = saved_line;
+        column_ = saved_column;
 
-char Lexer::peek() const {
-    if (isAtEnd()) return '\0';
-    return source_[current_];
-}
+        return token;
+    }
 
-char Lexer::peekNext() const {
-    if (current_ + 1 >= source_.length()) return '\0';
-    return source_[current_ + 1];
-}
+    bool Lexer::isAtEnd() const
+    {
+        return current_ >= source_.length();
+    }
 
-bool Lexer::match(char expected) {
-    if (isAtEnd()) return false;
-    if (source_[current_] != expected) return false;
-    advance();
-    return true;
-}
+    char Lexer::advance()
+    {
+        column_++;
+        return source_[current_++];
+    }
 
-void Lexer::skipWhitespace() {
-    while (!isAtEnd()) {
-        char c = peek();
-        if (c == ' ' || c == '\t' || c == '\r') {
-            advance();
-        } else if (c == '\n') {
-            line_++;
-            column_ = 0;
-            advance();
-        } else {
-            break;
+    char Lexer::peek() const
+    {
+        if (isAtEnd()) return '\0';
+        return source_[current_];
+    }
+
+    char Lexer::peekNext() const
+    {
+        if (current_ + 1 >= source_.length()) return '\0';
+        return source_[current_ + 1];
+    }
+
+    bool Lexer::match(char expected)
+    {
+        if (isAtEnd()) return false;
+        if (source_[current_] != expected) return false;
+        advance();
+        return true;
+    }
+
+    void Lexer::skipWhitespace()
+    {
+        while (!isAtEnd())
+        {
+            char c = peek();
+            if (c == ' ' || c == '\t' || c == '\r')
+            {
+                advance();
+            }
+            else if (c == '\n')
+            {
+                line_++;
+                column_ = 0;
+                advance();
+            }
+            else
+            {
+                break;
+            }
         }
     }
-}
 
-void Lexer::skipComment() {
-    while (!isAtEnd() && peek() != '\n') {
-        advance();
-    }
-}
-
-Token Lexer::makeToken(TokenType type, const std::string& value) {
-    return Token(type, value, SourceLocation(filename_, line_, column_));
-}
-
-Token Lexer::identifier() {
-    size_t start = current_;
-    int start_column = column_;
-    
-    while (!isAtEnd() && (std::isalnum(peek()) || peek() == '_')) {
-        advance();
-    }
-    
-    std::string text = source_.substr(start, current_ - start);
-    TokenType type = checkKeyword(text);
-    
-    return Token(type, text, SourceLocation(filename_, line_, start_column));
-}
-
-Token Lexer::number() {
-    size_t start = current_;
-    int start_column = column_;
-    bool is_float = false;
-    
-    while (!isAtEnd() && std::isdigit(peek())) {
-        advance();
-    }
-    
-    if (peek() == '.' && std::isdigit(peekNext())) {
-        is_float = true;
-        advance(); // consume '.'
-        while (!isAtEnd() && std::isdigit(peek())) {
+    void Lexer::skipComment()
+    {
+        while (!isAtEnd() && peek() != '\n')
+        {
             advance();
         }
     }
-    
-    std::string text = source_.substr(start, current_ - start);
-    TokenType type = is_float ? TokenType::FLOAT : TokenType::INTEGER;
-    
-    return Token(type, text, SourceLocation(filename_, line_, start_column));
-}
 
-Token Lexer::string() {
-    size_t start = current_;
-    int start_column = column_ - 1;
-    std::string value;
-    
-    while (!isAtEnd() && peek() != '"') {
-        if (peek() == '\\') {
+    Token Lexer::makeToken(TokenType type, const std::string& value)
+    {
+        return Token(type, value, SourceLocation(filename_, line_, column_));
+    }
+
+    Token Lexer::identifier()
+    {
+        size_t start = current_;
+        int start_column = column_;
+
+        while (!isAtEnd() && (std::isalnum(peek()) || peek() == '_'))
+        {
             advance();
-            if (!isAtEnd()) {
-                char escaped = advance();
-                switch (escaped) {
-                    case 'n': value += '\n'; break;
-                    case 't': value += '\t'; break;
-                    case 'r': value += '\r'; break;
-                    case '\\': value += '\\'; break;
-                    case '"': value += '"'; break;
+        }
+
+        std::string text = source_.substr(start, current_ - start);
+        TokenType type = checkKeyword(text);
+
+        return Token(type, text, SourceLocation(filename_, line_, start_column));
+    }
+
+    Token Lexer::number()
+    {
+        size_t start = current_;
+        int start_column = column_;
+        bool is_float = false;
+
+        while (!isAtEnd() && std::isdigit(peek()))
+        {
+            advance();
+        }
+
+        if (peek() == '.' && std::isdigit(peekNext()))
+        {
+            is_float = true;
+            advance(); // consume '.'
+            while (!isAtEnd() && std::isdigit(peek()))
+            {
+                advance();
+            }
+        }
+
+        std::string text = source_.substr(start, current_ - start);
+        TokenType type = is_float ? TokenType::FLOAT : TokenType::INTEGER;
+
+        return Token(type, text, SourceLocation(filename_, line_, start_column));
+    }
+
+    Token Lexer::string()
+    {
+        size_t start = current_;
+        int start_column = column_ - 1;
+        std::string value;
+
+        while (!isAtEnd() && peek() != '"')
+        {
+            if (peek() == '\\')
+            {
+                advance();
+                if (!isAtEnd())
+                {
+                    char escaped = advance();
+                    switch (escaped)
+                    {
+                    case 'n': value += '\n';
+                        break;
+                    case 't': value += '\t';
+                        break;
+                    case 'r': value += '\r';
+                        break;
+                    case '\\': value += '\\';
+                        break;
+                    case '"': value += '"';
+                        break;
                     default: value += escaped;
+                    }
                 }
             }
-        } else {
-            value += advance();
+            else
+            {
+                value += advance();
+            }
         }
-    }
-    
-    if (isAtEnd()) {
-        return makeToken(TokenType::INVALID, "Unterminated string");
-    }
-    
-    advance(); // closing "
-    
-    return Token(TokenType::STRING, value, SourceLocation(filename_, line_, start_column));
-}
 
-Token Lexer::charLiteral() {
-    int start_column = column_ - 1;
-    
-    if (isAtEnd()) {
-        return makeToken(TokenType::INVALID, "Unterminated char");
+        if (isAtEnd())
+        {
+            return makeToken(TokenType::INVALID, "Unterminated string");
+        }
+
+        advance(); // closing "
+
+        return Token(TokenType::STRING, value, SourceLocation(filename_, line_, start_column));
     }
-    
-    char c = advance();
-    if (c == '\\' && !isAtEnd()) {
-        char escaped = advance();
-        switch (escaped) {
-            case 'n': c = '\n'; break;
-            case 't': c = '\t'; break;
-            case 'r': c = '\r'; break;
-            case '\\': c = '\\'; break;
-            case '\'': c = '\''; break;
+
+    Token Lexer::charLiteral()
+    {
+        int start_column = column_ - 1;
+
+        if (isAtEnd())
+        {
+            return makeToken(TokenType::INVALID, "Unterminated char");
+        }
+
+        char c = advance();
+        if (c == '\\' && !isAtEnd())
+        {
+            char escaped = advance();
+            switch (escaped)
+            {
+            case 'n': c = '\n';
+                break;
+            case 't': c = '\t';
+                break;
+            case 'r': c = '\r';
+                break;
+            case '\\': c = '\\';
+                break;
+            case '\'': c = '\'';
+                break;
             default: c = escaped;
+            }
         }
-    }
-    
-    if (isAtEnd() || peek() != '\'') {
-        return makeToken(TokenType::INVALID, "Unterminated char");
-    }
-    
-    advance(); // closing '
-    
-    return Token(TokenType::CHAR, std::string(1, c), SourceLocation(filename_, line_, start_column));
-}
 
-TokenType Lexer::checkKeyword(const std::string& text) {
-    auto it = keywords.find(text);
-    if (it != keywords.end()) {
-        return it->second;
-    }
-    return TokenType::IDENTIFIER;
-}
+        if (isAtEnd() || peek() != '\'')
+        {
+            return makeToken(TokenType::INVALID, "Unterminated char");
+        }
 
+        advance(); // closing '
+
+        return Token(TokenType::CHAR, std::string(1, c), SourceLocation(filename_, line_, start_column));
+    }
+
+    TokenType Lexer::checkKeyword(const std::string& text)
+    {
+        auto it = keywords.find(text);
+        if (it != keywords.end())
+        {
+            return it->second;
+        }
+        return TokenType::IDENTIFIER;
+    }
 } // namespace pawc
-
-
-
